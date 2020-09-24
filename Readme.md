ce90a1801e507345a2c26e2523ca780d41741991
LautaroEst <lautaro.est@gmail.com>
2020-09-24 00:48:38 -0300

    Cambios en el main, se agregaron los .gitignore y se explica cómo hacer un gif.

--- Readme.md
+++ Readme.md
@@ -1,3 +1,17 @@
 # Trabajo Práctico 0 de Sistemas Embebidos
 
 El presente repositorio contiene los archivos correspondientes al TP0 de Sistemas Embebidos de FIUBA.
+
+## COnversión de imágenes a video
+
+Para convertir un conjunto de imágenes .pbm a un .gif animado, se sugiere hacerlo desde [esta](https://ezgif.com/maker) página.
+
+1. Seleccionar "Choose Files" y subir los archivos (hasta 100MB)
+
+2. Clickear en "Upload and make a GIF!"
+
+3. En la sección "GIF options", seleccionar un *Delay time* de 0.
+
+4. Clickear en "Make a GIF!"
+
+5. Seleccionar "Save" para descargar el gif.
--- src/main.c
+++ src/main.c
@@ -2,19 +2,40 @@
 #include <stdio.h>
 #include "image.h"
 
+
+#define MAX_IMG_NAME	20
+
+#define N_ROWS			200 	// Cantidad de filas
+#define N_COLS			200		// Cantidad de columnas
+
+
 /* Ejemplo de uso de las funciones de la clase "imagen" */
 
 int main(void){
 	
 	image_t img;
-	image_ctor(&img,200,200); // Construyo el objeto "imagen"
+	int img_count=0;
+	char img_name[MAX_IMG_NAME];
 
-	for(int i=0; i<img.n_cols; i++){
-		image_write(&img,i,i,HIGH); // Lo modifico como yo quiero
+	for(int l=10; l<180; l=l+2){
+
+		// Construyo el objeto "imagen"
+		image_ctor(&img,N_ROWS,N_COLS); 
+
+		// Lo modifico como yo quiera
+		for(int i=0; i<l; i++){
+			image_write(&img,i,i,HIGH); 
+		}
+		
+		// Lo guardo en un archivo
+		sprintf(img_name,"./images/img%.3d.pbm",img_count); 
+		image_to_file(&img,img_name); 
+		
+		// Libero memoria
+		image_dtor(&img); 
+
+		++img_count;
 	}
-
-	image_to_file(&img,"im.pbm"); // Lo guardo en un archivo
-	image_dtor(&img); // Libero memoria
 
 	return 0;
 }
